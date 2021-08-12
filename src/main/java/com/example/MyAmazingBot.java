540c0cea6b00e6c34d18a189be2617755e80227c
Perepelytsia <perepelytsa@devoler.com>
2021-08-13 01:53:27 +0300

    add component

--- src/main/java/com/example/MyAmazingBot.java
+++ src/main/java/com/example/MyAmazingBot.java
@@ -5,11 +5,13 @@
  */
 package com.example;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Component
 public class MyAmazingBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
